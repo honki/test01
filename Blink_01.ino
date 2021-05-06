eb5a3700c64120f22e6cd4efde20497e5411a41d
Honki <honkiaa@gmail.com>
2021-05-06 20:48:17 +0800

    test01

--- Blink_01.ino
+++ Blink_01.ino
@@ -1,5 +1,5 @@
 /*
-  Blink xxxxx
+  Blink
 
   Turns an LED on for one second, then off for one second, repeatedly.
 
@@ -33,5 +33,9 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);     // wait for a second
 }
